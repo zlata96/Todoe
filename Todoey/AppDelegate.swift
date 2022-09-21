44994d142f19bbe6d54173f736ffdf2797d4694e
Zlata Guseva <gusevazlata96@gmail.com>
2022-09-21 17:13:34 +0400

    Update CoreData

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -17,19 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
             return true
         }
-
-        // MARK: UISceneSession Lifecycle
-
-        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        }
-
-        func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        }
-
-        // MARK: - Core Data stack
-
-        lazy var persistentContainer: NSPersistentContainer = {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
             let container = NSPersistentContainer(name: "DataModel")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
@@ -52,7 +41,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 }
             }
         }
-
-
 }
 
